70c3e8c0e72c48c07d4b90f4a61d35a3ebf11ec1
Anthony Belardo <abelardo@regscale.com>
2023-02-04 07:56:08 -0500

    added seed data and method to populate db with seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,5 +29,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occured during migration.");
+    throw;
+}
 
 app.Run();
