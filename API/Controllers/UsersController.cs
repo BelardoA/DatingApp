46527cc54a4a9f136a1cae1166ac2c9de340e9ad
Anthony Belardo <abelardo@regscale.com>
2023-02-04 08:54:21 -0500

    implemented automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,21 +13,29 @@ namespace API.Controllers;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository;
+        _mapper = mapper;
     }
     
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        return Ok(await _userRepository.GetUsersAsync());
+        var users = await _userRepository.GetUsersAsync();
+        var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+        return Ok(usersToReturn);
     }
 
     [HttpGet("{userName}")]
-    public async Task<ActionResult<AppUser>> GetUsers(string userName)
+    public async Task<ActionResult<MemberDto>> GetUsers(string userName)
     {
-        return await _userRepository.GetUserByUserNameAsync(userName);
+        var user = await _userRepository.GetUserByUserNameAsync(userName);
+
+        return _mapper.Map<MemberDto>(user);
+
     }
 }
