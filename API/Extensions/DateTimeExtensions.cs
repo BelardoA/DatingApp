22498b372f0decc7befe777fd64f7816058ea501
Anthony Belardo <abelardo@regscale.com>
2023-02-04 07:30:09 -0500

    extended users

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Extensions;
+
+[Table("Photos")]
+public static class DateTimeExtensions
+{
+    
+    public static int CalculateAge(this DateOnly dob)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age = today.Year - dob.Year;
+
+        if (dob > today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
