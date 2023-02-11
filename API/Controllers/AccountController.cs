093e83cd33fd0fe7ea3d1cb5827307810f089008
BelardoA <livintodie4e@gmail.com>
2023-02-11 16:53:35 -0500

    added user image next to welcome message

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,7 +49,9 @@ public class AccountController : BaseApiController
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+        var user = await _context.Users
+            .Include(x=> x.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
         if (user == null) return Unauthorized("Invalid username.");
         
@@ -65,7 +67,8 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             UserName = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.First(x => x.IsMain)?.Url
         };
     }
 
