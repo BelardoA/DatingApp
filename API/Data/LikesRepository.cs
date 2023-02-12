99ba3d2adf50ae9f8e1aff219962f845dc04f3c7
BelardoA <livintodie4e@gmail.com>
2023-02-12 12:02:05 -0500

    added like functionality in application

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,57 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class LikesRepository : ILikesRepository
+{
+    private readonly DataContext _context;
+
+    public LikesRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+    {
+        return await _context.Likes.FindAsync(sourceUserId, targetUserId);
+    }
+
+    public async Task<AppUser> GetUserWithLikes(int userId)
+    {
+        return await _context.Users
+            .Include(x => x.LikedUsers)
+            .FirstOrDefaultAsync(x => x.Id == userId);
+    }
+
+    public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+    {
+        var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+        var likes = _context.Likes.AsQueryable();
+
+        if (predicate == "liked")
+        {
+            likes = likes.Where(like => like.SourceUserId == userId);
+            users = likes.Select(like => like.TargetUser);
+        }
+
+        if (predicate == "likedBy")
+        {
+            likes = likes.Where(like => like.TargetUserId == userId);
+            users = likes.Select(like => like.SourceUser);
+        }
+
+        return await users.Select(u => new LikeDto
+        {
+            UserName = u.UserName,
+            KnownAs = u.KnownAs,
+            Age = u.DateOfBirth.CalculateAge(),
+            City = u.City,
+            PhotoUrl = u.Photos.FirstOrDefault(x => x.IsMain).Url,
+            Id = u.Id
+        }).ToListAsync();
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,7 +9,7 @@ public class AppUser
     public string KnownAs { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
-    public DateOnly DateOfBirth{ get; set; }
+    public DateOnly DateOfBirth { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime LastActive { get; set; } = DateTime.UtcNow;
     public string Gender { get; set; }
@@ -19,9 +19,6 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
-
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
 }
