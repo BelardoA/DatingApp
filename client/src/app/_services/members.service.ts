5e53cbc3c007bdda395c32a8ae187da1ce35c0f6
BelardoA <livintodie4e@gmail.com>
2023-02-12 01:17:57 -0500

    fixed caching for member details

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,11 +12,16 @@ import {UserParams} from "../_modules/userParams";
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
   constructor(private http: HttpClient) {
   }
 
   getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if (response) return of(response);
+
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
     params = params.append('minAge', userParams.minAge);
@@ -24,7 +29,12 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    )
   }
 
   private getPaginatedResults<T>(url: string, params: HttpParams) {
