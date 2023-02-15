45f21829f997c357ff3ac2656007427103bdf3de
BelardoA <livintodie4e@gmail.com>
2023-02-15 17:49:21 -0500

    added messages to angular side

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {getPaginatedResults, getPaginationHeaders} from "./PaginationHelper";
+import {Message} from "../_models/message";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) {
+  }
+
+  getMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append("Container", container);
+    return getPaginatedResults<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+}
