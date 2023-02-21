c2a1da248ea61552a8383a467a4027153b3d1a52
BelardoA <livintodie4e@gmail.com>
2023-02-21 07:19:05 -0500

    added user roles functionality

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,10 +37,14 @@ public class AccountController : BaseApiController
 
         if (!result.Succeeded) return BadRequest(result.Errors);
 
+        var roleResults = await _userManager.AddToRoleAsync(user, "Member");
+
+        if (!roleResults.Succeeded) return BadRequest(result.Errors);
+
         return new UserDto
         {
             UserName = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             KnownAs = user.KnownAs,
             Gender = user.Gender
         };
@@ -62,7 +66,7 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             UserName = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
             KnownAs = user.KnownAs,
             Gender = user.Gender
