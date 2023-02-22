529ab019db697011989eaed041acf45e7e05fe49
BelardoA <livintodie4e@gmail.com>
2023-02-22 08:46:45 -0500

    added online indicators for users

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../environments/environment";
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {ToastrService} from "ngx-toastr";
 import {User} from "../_models/user";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ import {User} from "../_models/user";
 export class PresenceService {
   hubUrl = environment.hubUrl;
   private hubConnection?: HubConnection;
+  private onlineUsersSource = new BehaviorSubject<string[]>([]);
+  onlineUsers$ = this.onlineUsersSource.asObservable();
 
   constructor(private toastr: ToastrService) {
   }
@@ -30,6 +33,10 @@ export class PresenceService {
 
     this.hubConnection.on('UserIsOffline', username => {
       this.toastr.warning(username + 'is now offline.');
+    })
+
+    this.hubConnection.on("GetOnlineUsers", userNames => {
+      this.onlineUsersSource.next(userNames);
     })
   }
 
