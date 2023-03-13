2bd6d5b60483346755f6da7d13bf1105949fc49d
BelardoA <livintodie4e@gmail.com>
2023-03-12 22:07:20 -0400

    added userid and username to PhotoDto.cs

--- API/DTOs/PhotoDto.cs
+++ API/DTOs/PhotoDto.cs
@@ -1,8 +1,12 @@
+using API.Entities;
+
 namespace API.DTOs;
 
 public class PhotoDto
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
+    public string UserName { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public bool IsApproved { get; set; }
