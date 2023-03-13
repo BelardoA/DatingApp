99be1b86a21ce2a8d2419b73010183350ef71ae8
BelardoA <livintodie4e@gmail.com>
2023-03-12 23:06:15 -0400

    implemented approving and deleting photos in the admin portal

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {AdminService} from "../../_services/admin.service";
+import {Photo} from "../../_models/photo";
 
 @Component({
   selector: 'app-photo-management',
@@ -6,11 +10,36 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  unapprovedPhotos: Photo[] = [];
 
-  constructor() {
+  constructor(private http: HttpClient, private adminService: AdminService) {
   }
 
   ngOnInit(): void {
+    this.getUnapprovedPhotos().subscribe(photos => {
+      this.unapprovedPhotos = photos;
+    })
   }
 
+  getUnapprovedPhotos() {
+    return this.http.get<Photo[]>(this.baseUrl + 'admin/photos-to-moderate');
+  }
+
+  approvePhoto(photoId: number) {
+    this.adminService.approvePhoto(photoId).subscribe({
+        next: () => {
+          this.unapprovedPhotos = this.unapprovedPhotos.filter(x => x.id != photoId);
+        }
+      }
+    )
+  }
+
+  deletePhoto(photoId: number) {
+    this.adminService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.unapprovedPhotos = this.unapprovedPhotos.filter(x => x.id != photoId);
+      }
+    })
+  }
 }
